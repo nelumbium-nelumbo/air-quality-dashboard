320df03c36a16f178534a55f0168dac3c6a962f1
nelumbium-nelumbo <58062795+nelumbium-nelumbo@users.noreply.github.com>
2024-03-06 13:34:51 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -260,12 +260,12 @@ with col2_4:
 
 tab2_1, tab2_2 = st.tabs(["Measured Pollutants", "Wind Speed & Direction"])
 with tab2_1:
-    x_polutants = len(main_aqi.iloc[len(main_aqi)-1,:5].sort_values())
+    x_polutants = len(main_aqi.iloc[len(main_aqi)-1,:5].sort_values(ascending=False).index)
     explodelist2 = [0.1, 0, 0, 0, 0]
-    colorsss = custom_colors(main_aqi.iloc[len(main_aqi)-1,:5].sort_values(), restcol = restcols[:x_polutants])
+    colorsss = custom_colors(main_aqi.iloc[len(main_aqi)-1,:5].sort_values(ascending=False), restcol = restcols[:x_polutants])
     st.subheader("Measured Pollutants 24-hours (based on AQI)")
     fig4, ax4 = plt.subplots()
-    ax4.pie(main_aqi.iloc[len(main_aqi)-1,:5], labels=main_aqi.iloc[len(main_aqi)-1,:5].index,
+    ax4.pie(main_aqi.iloc[len(main_aqi)-1,:5].sort_values(ascending=False), labels=main_aqi.iloc[len(main_aqi)-1,:5].sort_values(ascending=False).index,
             autopct='%1.1f%%', colors=colorsss, explode = explodelist2[:x_polutants], textprops={'fontsize': 10})
     st.pyplot(fig4)
     
